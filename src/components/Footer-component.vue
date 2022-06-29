a31b01b97ec07ebe406a5157ab9ce51a773d8cf9
unknown <edegleyson.2013@gmail.com>
2022-06-29 03:38:43 -0300

    Iniciado projeto city-climate

---
+++ src/components/Footer-component.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer>
+        
+    </footer>
+</template>
+
+<script>
+
+export default {
+    name: 'FooterComponent',
+    data(){
+        return{
+           
+        }
+    },
+}
+</script>
+
+<style scoped>
+    footer{
+        height: 60px;
+        width: 100%;
+        background-color: var(--color-background-nav);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ src/components/Header-component.vue
@@ -0,0 +1,63 @@
+<template>
+    <header>
+        <nav class="container">
+            <div id="menu-items">
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+}
+</script>
+
+<style scoped>
+header {
+    background-color: var(--color-background-nav);
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    height: 60px;
+    align-items: center;
+}
+
+
+#menu-items {
+    position: fixed;
+    top: 0;
+    right: 0;
+    background-color: var(--color-background-nav);
+    width: 60%;
+    height: 100vh;
+    display: none;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+ul {
+    list-style: none;
+    text-align: center;
+    height: 60px;
+    align-items: center;
+}
+
+ul li {
+    margin: 20px 0px;
+    color: var(--color-text-light);
+}
+</style>
